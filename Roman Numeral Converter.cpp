265f7fb8e2eef5a081f55f2d3fea9f7efb9f237f
mrust62 <111906550+mrust62@users.noreply.github.com>
2024-08-22 17:59:47 -0400

    Math is correct still need to add exceptions for when the Roman numeral is input wrong.

--- Roman Numeral Converter.cpp
+++ Roman Numeral Converter.cpp
@@ -10,21 +10,111 @@ using namespace std;
 int main()
 {
     string romanNumeral = "A";
-    
-    
+    string romanTemp;
+    int convertedValue = 0;
+
+
     cout << "Please enter the Roman Numeral you want converted. (in all Caps)\n";
     cin >> romanNumeral;
 
     int romanSize = romanNumeral.length();
 
     cout << "The Numeral was: " << romanNumeral.length() << endl;
-
+    
     
     for (int i = romanNumeral.length() - 1; i >= 0; i--)
     {
         cout << "Letter" << i << ": " << romanNumeral[i] << endl;
-    }
+        romanTemp = romanNumeral[i];
 
+        
+
+        if (romanTemp.compare("I") == 0)
+        {
+            convertedValue += 1;
+            
+        }
+        else if (romanTemp.compare("V") == 0)
+        {
+            
+            if (romanTemp.compare("I") == 0)
+            {
+                convertedValue += 4;
+                i--;
+            }
+            else
+            {
+                convertedValue += 5;
+            }
+        }
+        else if (romanTemp.compare("X") == 0)
+        {
+            romanTemp = romanNumeral[i - 1];
+            if (romanTemp.compare("I") == 0)
+            {
+                convertedValue += 9;
+                i--;
+            }
+            else
+            {
+                convertedValue += 10;
+            }
+        }
+        else if (romanTemp.compare("L") == 0)
+        {
+            romanTemp = romanNumeral[i - 1];
+            if (romanTemp.compare("X") == 0)
+            {
+                convertedValue += 40;
+                i--;
+            }
+            else
+            {
+                convertedValue += 50;
+            }
+        }
+        else if (romanTemp.compare("C") == 0)
+        {
+            romanTemp = romanNumeral[i - 1];
+            if (romanTemp.compare("X") == 0)
+            {
+                convertedValue += 90;
+                i--;
+            }
+            else
+            {
+                convertedValue += 100;
+            }
+        }
+        else if (romanTemp.compare("D") == 0)
+        {
+            romanTemp = romanNumeral[i - 1];
+            if (romanTemp.compare("C") == 0)
+            {
+                convertedValue += 400;
+                i--;
+            }
+            else
+            {
+                convertedValue += 500;
+            }
+        }
+        else if (romanTemp.compare("M") == 0)
+        {
+            romanTemp = romanNumeral[i];
+            if (romanTemp.compare("C") == 0)
+            {
+                convertedValue += 900;
+                i--;
+            }
+            else
+            {
+                convertedValue += 1000;
+                
+            }
+        }
+    }
+    cout << convertedValue;
     
 }
 
