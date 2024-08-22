6b63f32ec1c4d2e818fbc4ff73d8d7ea427edfec
mrust62 <111906550+mrust62@users.noreply.github.com>
2024-08-22 16:33:36 -0400

    Add project files.

---
+++ Roman Numeral Converter.cpp
@@ -0,0 +1,26 @@
+//Roman Numeral Converter
+//Written By Matthew Rust, 8/22/2024
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    string romanNumeral = "A";
+    
+    
+    cout << "Please enter the Roman Numeral you want converted. (in all Caps)\n";
+    cin >> romanNumeral;
+
+    int num = romanNumeral.length();
+
+    cout << "The Numeral was: " + num;
+
+    string romanSplit[] = {"a"};
+
+
+}
+
